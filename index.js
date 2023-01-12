4685dad543b1546c570a3c332a81cc7a132a96cf
rishan3 <muhammedrishank0@gmail.com>
2023-01-12 11:46:49 +0530

    Counter

---
+++ index.js
@@ -0,0 +1,17 @@
+const count = document.getElementById("count");
+leta=0;
+function resetcount(){
+  a=0;
+  count.innerText=a;
+  count.style.color="black";
+}
+function increasecount(){
+   count.innerText=++a;
+   a>0 ? count.style.color="green": 
+   count.style.color="red";
+}
+function decreasecount(){
+   count.innerText=--a;
+   a>0 ? count.style.color="green": 
+  count.style.color="red";
+}
